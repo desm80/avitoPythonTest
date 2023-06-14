44f8bde7b93edf13a8c1a853f8e7f0b0be559530
desm <desm80@yandex.ru>
2023-06-14 11:58:20 +0300

    Небольшой рефакторинг, разнес по отдельным функциям чтение файла и обработку полученного ответа.

--- main.py
+++ main.py
@@ -8,30 +8,30 @@ from typing import List
 SOURCE_URL = 'https://raw.githubusercontent.com/avito-tech/python-trainee-assignment/main/matrix.txt'
 
 
-async def read_file(filename: str) -> List[List[int]]:
+async def read_file(filename: str) -> str:
     async with aiohttp.ClientSession() as session:
         async with session.get(filename) as response:
-            matrix = await response.text()
-            result = []
-            result_matrix = []
-            len_matrix = int(len(re.findall(r'\b\d+\b', matrix)) ** 0.5)
-            for item in matrix.split(' '):
-                if item.strip().isdigit():
-                    if len(result) < len_matrix:
-                        result.append(int(item))
-                    else:
-                        result_matrix.append(result)
-                        result = []
-                        result.append(int(item))
-            result_matrix.append(result)
-            return result_matrix
+            return await response.text()
 
 
-async def get_matrix(url: str) -> List[int]:
-    matrix_in = await read_file(url)
-    # Для обхода матрицы против часовой стрелки повернем ее на 90 градусов,
-    # чтобы колонка стала строкой
-    matrix = [list(row) for row in list(zip(*matrix_in))[::-1]]
+def text_to_matrix(text: str) -> List[List[int]]:
+    result = []
+    result_matrix = []
+    len_matrix = int(len(re.findall(r'\b\d+\b', text)) ** 0.5)
+    for item in text.split(' '):
+        if item.strip().isdigit():
+            if len(result) < len_matrix:
+                result.append(int(item))
+            else:
+                result_matrix.append(result)
+                result = []
+                result.append(int(item))
+    result_matrix.append(result)
+    return result_matrix
+
+
+def read_matrix_counterclock_wise(matrix: List[List[int]]) -> List[int]:
+    matrix = [list(row) for row in list(zip(*matrix))[::-1]]
     result = []
     while True:
         matrix.reverse()
@@ -45,6 +45,13 @@ async def get_matrix(url: str) -> List[int]:
         for item in row:
             lst.append(item)
     return lst
+
+
+async def get_matrix(url: str) -> List[int]:
+    response = await read_file(url)
+    matrix_in = text_to_matrix(response)
+    return read_matrix_counterclock_wise(matrix_in)
+
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
