d669c81409cb11b781cabe4cfd2a79782ad3fb8b
desm <desm80@yandex.ru>
2023-06-16 10:58:55 +0300

    Рефакторинг. Подготовка создания библиотеки.

--- setup.py
+++ setup.py
@@ -8,5 +8,5 @@ setup(name='read_matrix_counterclock_wise',
       author_email='jr-01@rambler.ru',
       description='Read matrix counterclock-wise from text file',
       packages=find_packages(exclude=['test']),
-      long_description=open('README.md').read(),
+      long_description=open('README.md', encoding="utf-8").read(),
       zip_safe=False)
